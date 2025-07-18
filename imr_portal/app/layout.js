d54d0306b3868c2b45836bc5223208736635257a
Harry-SAIT <chinglok.chu@edu.sait.ca>
2025-07-18 13:48:37 -0600

    Fix Tailwind not working in this project.

--- imr_portal/app/layout.js
+++ imr_portal/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import React from 'react';
+import './globals.css'
 
 export const metadata = {
   title: 'IMR Movie Portal',
--- imr_portal/postcss.config.mjs
+++ imr_portal/postcss.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
 
-
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+export default config;
